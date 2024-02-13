42d5ca28e47287d5c333f3ea930334cc8f0991f2
Mashreka <mashreka.mannan@gmail.com>
2024-02-13 03:09:16 +0100

    basic changes

--- src/main/java/Servlets/AllStudentsServlet.java
+++ src/main/java/Servlets/AllStudentsServlet.java
@@ -28,6 +28,7 @@ public class AllStudentsServlet extends HttpServlet {
             out.println("<html><head><title>Grit Academy</title><link rel='stylesheet' type='text/css' href='/css/styles.css'></head>");
             out.println("<body>");
             out.println("<div class='navbar'>");
+            out.println("<a href='index.html'>Home</a>");
             out.println("<a href='/allStudents' class='active'>All Students</a>");
             out.println("<a href='/allCourses'>All Courses</a>");
             out.println("<a href='/studentCourses'>All Students with Courses</a>");
